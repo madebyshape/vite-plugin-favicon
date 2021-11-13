447ee4f82a2a01edf54b51fd5a2e3ceeb1c26737
Andrew Welch <andrew@nystudio107.com>
2021-11-12 23:20:52 -0500

    feat: Un-deprecated the `outputPath` config setting, to allow you to specify where the output assets are saved, relative to the Vite output directory
    Signed-off-by: Andrew Welch <andrew@nystudio107.com>

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ export type ViteFaviconsPluginOptions = {
 		@default "assets/logo.png"
 	 */
 	logo?: string,
-	/** Inject html links/metadata.
+	/** Inject html links/metadata -- set to `false` to generate a webapp.html` file.
 		@default true
 	 */
 	inject?: boolean,
@@ -25,6 +25,9 @@ export type ViteFaviconsPluginOptions = {
 		@default process.cwd()
 	 */
 	projectRoot?: string,
+	/** Output Path for the favicon images & files, relative to the Vite output directory
+	 */
+	outputPath?: string,
 	/** prefix is delegated to Rollup/Vite (keeping for people migrating from Webpack)
 	 * @deprecated
 	*/
@@ -37,10 +40,6 @@ export type ViteFaviconsPluginOptions = {
 	 * @deprecated
 	*/
 	publicPath?: string,
-	/** Output Path is delegated to Rollup/Vite (keeping for people migrating from Webpack)
-	 * @deprecated
-	*/
-	outputPath?: string,
 }
 
 class HtmlTag implements HtmlTagDescriptor {
@@ -57,6 +56,7 @@ type FaviconsPluginArgs = Partial<ViteFaviconsPluginOptions> | ViteFaviconsPlugi
 export const ViteFaviconsPlugin = (options: FaviconsPluginArgs = {} ): Plugin => {
 	let viteConfig: ResolvedConfig;
 	const lOptions = typeof options === 'string' ? {logo:options} : options;
+	lOptions.outputPath = lOptions.outputPath === undefined ? '' : lOptions.outputPath;
 	lOptions.inject = lOptions.inject === undefined ? true : lOptions.inject;
 	lOptions.projectRoot = lOptions.projectRoot === undefined ? process.cwd() : lOptions.projectRoot;
 	const LOGO_PATH = path.resolve(lOptions.logo || path.join('assets','logo.png'));
@@ -93,15 +93,18 @@ export const ViteFaviconsPlugin = (options: FaviconsPluginArgs = {} ): Plugin =>
 		// Only emit files if we're doing a build
 		if (viteConfig.command === 'build') {
 			for (const {name, contents} of res.files) {
-				assetIds.set(name, ctx.emitFile({type: 'asset', fileName: name, source: contents}));
+				const filePath = path.join(lOptions.outputPath === undefined ? '' : lOptions.outputPath, name);
+				assetIds.set(name, ctx.emitFile({type: 'asset', fileName: filePath, source: contents}));
 			}
 			for (const {name, contents} of res.images) {
-				assetIds.set(name, ctx.emitFile({type: 'asset', fileName: name, source: contents}));
+				const filePath = path.join(lOptions.outputPath === undefined ? '' : lOptions.outputPath, name);
+				assetIds.set(name, ctx.emitFile({type: 'asset', fileName: filePath, source: contents}));
 			}
 			if (!lOptions.inject) {
 				const name = 'webapp.html';
 				const contents = res.html.join("\n");
-				assetIds.set(name, ctx.emitFile({type: 'asset', fileName: name, source: contents}));
+				const filePath = path.join(lOptions.outputPath === undefined ? '' : lOptions.outputPath, name);
+				assetIds.set(name, ctx.emitFile({type: 'asset', fileName: filePath, source: contents}));
 			}
 		}
 		// Parse the HTML into tag objects for later injection
